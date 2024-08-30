52730a51ade73164c99f9de651ed360a2fbf0876
mokkun55 <mokkunpc@gmail.com>
2024-08-31 00:41:30 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const userNameSelector = "[data-testid='UserName']";
+
+const div = document.createElement("div");
+div.innerHTML = `
+  <button>ボタンだよ</button>
+`;
+
+const main = () => {
+  // 特定の要素が存在するかどうかを確認するタイマーを設定
+  const jsInitCheckTimer = setInterval(jsLoaded, 500);
+
+  // 要素が読み込まれるかどうかを確認する関数
+  function jsLoaded() {
+    // 取得したい要素をセレクタで取得
+    const targetElement = document.querySelector(userNameSelector);
+
+    // 要素が見つかった場合の処理
+    if (targetElement != null) {
+      clearInterval(jsInitCheckTimer); // タイマーを停止
+      console.log("要素が見つかりました");
+
+      // ここに処理を書く
+      targetElement.appendChild(div);
+    } else {
+      console.count("要素がまだ見つかりません");
+    }
+  }
+};
+
+window.addEventListener("load", main, false);
