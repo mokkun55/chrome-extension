b5e0fd24f18870ce800abbf73be47e231bc321b3
mokkun55 <mokkunpc@gmail.com>
2024-08-31 23:30:11 +0900

    仕様変更

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
-const userNameSelector = "[data-testid='UserName']";
-
 const div = document.createElement("div");
 div.innerHTML = `
-  <button id="viewBtn">メディアビュアー</button>
-`;
-div.style.cssText = `
-  margin-left: auto;
-`;
-
-const viewButton = div.querySelector("#viewBtn");
-viewButton.style.cssText = `
-  background-color: #fff;
-  border-radius: 9999px;
-  border: 1px solid #ccc;
-  padding: 0.5rem 1rem;
-  margin-left: auto;
-  font-weight: bold;
-  cursor: pointer;
+  <p>Hello, World!!</p>
 `;
 
 const main = () => {
@@ -26,7 +10,7 @@ const main = () => {
   // 要素が読み込まれるかどうかを確認する関数
   function jsLoaded() {
     // 取得したい要素をセレクタで取得
-    const targetElement = document.querySelector(userNameSelector);
+    const targetElement = document.querySelector("img");
 
     // 要素が見つかった場合の処理
     if (targetElement != null) {
@@ -34,13 +18,6 @@ const main = () => {
       console.log("要素が見つかりました");
 
       // ここに処理を書く
-      // ボタンを追加
-      targetElement.appendChild(div);
-
-      // ボタンをクリックしたときの処理
-      viewButton.addEventListener("click", () => {
-        console.log("ボタンがクリックされました");
-      });
     } else {
       console.count("要素がまだ見つかりません");
     }
