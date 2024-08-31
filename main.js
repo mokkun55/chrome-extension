0672691ead8603093cb2d7f3daec247bd0d5ecb9
mokkun55 <mokkunpc@gmail.com>
2024-08-31 22:48:14 +0900

    ボタンのスタイル

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ const userNameSelector = "[data-testid='UserName']";
 
 const div = document.createElement("div");
 div.innerHTML = `
-  <button>ボタンだよ</button>
+  <button id="viewBtn">メディアビュアー</button>
+`;
+div.style.cssText = `
+  margin-left: auto;
+`;
+
+const viewButton = div.querySelector("#viewBtn");
+viewButton.style.cssText = `
+  background-color: #fff;
+  border-radius: 9999px;
+  border: 1px solid #ccc;
+  padding: 0.5rem 1rem;
+  margin-left: auto;
+  font-weight: bold;
+  cursor: pointer;
 `;
 
 const main = () => {
@@ -20,7 +34,13 @@ const main = () => {
       console.log("要素が見つかりました");
 
       // ここに処理を書く
+      // ボタンを追加
       targetElement.appendChild(div);
+
+      // ボタンをクリックしたときの処理
+      viewButton.addEventListener("click", () => {
+        console.log("ボタンがクリックされました");
+      });
     } else {
       console.count("要素がまだ見つかりません");
     }
